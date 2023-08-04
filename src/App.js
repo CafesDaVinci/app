38047a195b0de3f0bcb64dc0e9721cb3ef48b4d1
hebertdev82 <hebertdev82@gmail.com>
2023-08-04 14:51:09 -0300

    catalogopdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo512.png';
 import './App.css';
-import Pdf from './pdf/catalogo.pdf';
 
 function App() {
   return (
@@ -8,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h5>Nosso site está em construção</h5>
-        <a href={Pdf} target="_blank" rel="noreferrer" className='linkpdf'>Confira nosso Catálogo de Cafés 2023</a>
+        <a href="/catalogo.pdf" target="_blank" rel="noopener noreferrer" className='linkpdf'>Confira nosso Catálogo de Cafés 2023</a>
       </header>
     </div>
   );
